462588e2110a73ee9ff95fcf68da651fc00ddae4
Jose Carlos <josecarlospas1@gmail.com>
2023-01-05 18:47:01 -0600

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import loading from './animation_500_lcjsdwvh.gif'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={loading} alt="loading..." width="500vw"/>
+        <p>Estamos en proceso de construcción</p>
       </header>
     </div>
   );
