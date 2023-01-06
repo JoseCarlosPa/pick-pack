aa291ea526d2cf8f19c20c1e01c9526ff74c97f7
josecarlos <josecarlospas1@gmail.com>
2023-01-06 14:13:04 -0600

    feat(middle): add middle pages

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -8,48 +8,42 @@ import DropDown from "./DropDown";
 const Navbar = () => {
 
   return (
-    <div className="flex flex-row">
-      <div className="flex flex-row mx-52 mt-0 pt-0">
-        <div className="flex flex-row -mt-1">
-          <img src={logoTitle} alt="logo" className="w-full h-16"/>
-          <img src={logoCorp} alt="logo" className="w-full pt-6"/>
-        </div>
-      </div>
-      <div className="flex flex-row w-full  text-xl font-bold mt-4">
-        <NavbarItem title="Inicio"/>
-        <DropDown title="Productos" >
-          <p className="hover:bg-gray-300 cursor-pointer">DUNNAGE</p>
-          <p className="hover:bg-gray-300 cursor-pointer">CAJAS</p>
-        </DropDown>
-        <DropDown title="Servicios">
-          <p className="hover:bg-gray-300 cursor-pointer">DISEÑO</p>
-          <p className="hover:bg-gray-300 cursor-pointer">PROTOTIPOS</p>
-          <p className="hover:bg-gray-300 cursor-pointer">PRODUCCIÓN</p>
-          <p className="hover:bg-gray-300 cursor-pointer">PRUEBAS</p>
-          <p className="hover:bg-gray-300 cursor-pointer">RETORNABLES</p>
-          <p className="hover:bg-gray-300 cursor-pointer">PROTECCIÓN</p>
-        </DropDown>
-        <DropDown title="Materiales">
-          <p className="hover:bg-gray-300 cursor-pointer">EPE</p>
-          <p className="hover:bg-gray-300 cursor-pointer">EPP</p>
-          <p className="hover:bg-gray-300 cursor-pointer">CROSS LINKED</p>
-          <p className="hover:bg-gray-300 cursor-pointer">POLIRREA</p>
-          <p className="hover:bg-gray-300 cursor-pointer">POLIURETANO</p>
-          <p className="hover:bg-gray-300 cursor-pointer">CARTÓN CORRUGADO</p>
-          <p className="hover:bg-gray-300 cursor-pointer">CARTÓN PLÁSTICO</p>
-        </DropDown>
-        <DropDown title="Procesos" >
-          <p className="hover:bg-gray-300 cursor-pointer">CNC</p>
-          <p className="hover:bg-gray-300 cursor-pointer">SUAJADO</p>
-          <p className="hover:bg-gray-300 cursor-pointer">HOTMELT</p>
-          <p className="hover:bg-gray-300 cursor-pointer">INYECCIÓN POLIURETANO</p>
-          <p className="hover:bg-gray-300 cursor-pointer">LAMINADO</p>
-          <p className="hover:bg-gray-300 cursor-pointer">HOJEADO</p>
-        </DropDown>
-        <NavbarItem title="Contacto"/>
-        <div className="">
-          <NavbarItem title="COTIZA CON NOSOTROS" className="ml-24 p-4 rounded-full border-2 border-white -mt-4 text-sm hover:bg-white hover:text-black cursor-pointer "/>
-        </div>
+    <div className="flex flex-row w-full text-xl font-bold mt-4 w-full justify-center">
+      <img src={logoTitle} alt="logo" className="mr-20 -mt-5"/>
+      <NavbarItem title="Inicio"/>
+      <DropDown title="Productos">
+        <p className="hover:bg-gray-300 cursor-pointer">DUNNAGE</p>
+        <p className="hover:bg-gray-300 cursor-pointer">CAJAS</p>
+      </DropDown>
+      <DropDown title="Servicios">
+        <p className="hover:bg-gray-300 cursor-pointer">DISEÑO</p>
+        <p className="hover:bg-gray-300 cursor-pointer">PROTOTIPOS</p>
+        <p className="hover:bg-gray-300 cursor-pointer">PRODUCCIÓN</p>
+        <p className="hover:bg-gray-300 cursor-pointer">PRUEBAS</p>
+        <p className="hover:bg-gray-300 cursor-pointer">RETORNABLES</p>
+        <p className="hover:bg-gray-300 cursor-pointer">PROTECCIÓN</p>
+      </DropDown>
+      <DropDown title="Materiales">
+        <p className="hover:bg-gray-300 cursor-pointer">EPE</p>
+        <p className="hover:bg-gray-300 cursor-pointer">EPP</p>
+        <p className="hover:bg-gray-300 cursor-pointer">CROSS LINKED</p>
+        <p className="hover:bg-gray-300 cursor-pointer">POLIRREA</p>
+        <p className="hover:bg-gray-300 cursor-pointer">POLIURETANO</p>
+        <p className="hover:bg-gray-300 cursor-pointer">CARTÓN CORRUGADO</p>
+        <p className="hover:bg-gray-300 cursor-pointer">CARTÓN PLÁSTICO</p>
+      </DropDown>
+      <DropDown title="Procesos">
+        <p className="hover:bg-gray-300 cursor-pointer">CNC</p>
+        <p className="hover:bg-gray-300 cursor-pointer">SUAJADO</p>
+        <p className="hover:bg-gray-300 cursor-pointer">HOTMELT</p>
+        <p className="hover:bg-gray-300 cursor-pointer">INYECCIÓN POLIURETANO</p>
+        <p className="hover:bg-gray-300 cursor-pointer">LAMINADO</p>
+        <p className="hover:bg-gray-300 cursor-pointer">HOJEADO</p>
+      </DropDown>
+      <NavbarItem title="Contacto"/>
+      <div className="">
+        <NavbarItem title="COTIZA CON NOSOTROS"
+                    className="p-4 rounded-full border-2 border-white -mt-4 text-sm hover:bg-white hover:text-black cursor-pointer "/>
       </div>
     </div>
   );
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import '../assets/css/main.css'
 import Navbar from "../components/Navbar";
+import {solid} from "@fortawesome/fontawesome-svg-core/import.macro";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 
 const Home = () => {
   return (
@@ -9,7 +11,29 @@ const Home = () => {
       <div className="flex flex-col main-bg">
         <Navbar/>
         <hr className="border-white my-2"/>
+        <div className="mt-80">
+          <div className="flex flex-col text-white text-center">
+            <h2 className="typo-nuevas">Nuevas</h2>
+            <h1 className="typo-tecnologias -mt-12">TECNOLOGÍAS</h1>
+            <p className="typo-parragraph -mt-2">Comenzando nuevos proyectos seguros de que lo que</p>
+            <p className="typo-parragraph -mt-3">nosotros tenemos para ti, te va a servir.</p>
+          </div>
+        </div>
+        <div className="w-full flex flex-row justify-center mt-8">
+          <div
+            className="border-2 bg-white rounded-full flex flex-row w-40 px-3 py-3 hover:bg-transparent hover:text-white cursor-pointer">
+            <p>Ver productos</p>
+            <FontAwesomeIcon icon={solid('caret-right')} className="ml-1 mt-1 cursor-pointer w-4"/>
+          </div>
+        </div>
       </div>
+      <div className="mt-4">
+
+      </div>
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+        <path fill="#f3f4f5" fill-opacity="1"
+              d="M0,128L80,133.3C160,139,320,149,480,144C640,139,800,117,960,122.7C1120,128,1280,160,1360,176L1440,192L1440,320L1360,320C1280,320,1120,320,960,320C800,320,640,320,480,320C320,320,160,320,80,320L0,320Z"></path>
+      </svg>
     </div>
   );
 }
